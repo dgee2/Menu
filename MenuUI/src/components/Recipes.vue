7239bf670bff1c48fbec9d4db6e4226c66e04a4e
Daniel Gee <dan@dan-gee.co.uk>
2023-06-14 17:52:21 +0100

    Fix type for recipe headers

--- MenuUI/src/components/Recipes.vue
+++ MenuUI/src/components/Recipes.vue
@@ -8,11 +8,14 @@ import { computed, onMounted } from "vue";
 import { ref } from "vue";
 import { VDataTable } from "vuetify/labs/VDataTable";
 
+type UnwrapReadonlyArrayType<A> = A extends Readonly<Array<infer I>> ? UnwrapReadonlyArrayType<I> : A
+type ReadonlyDataTableHeader = UnwrapReadonlyArrayType<InstanceType<typeof VDataTable>['headers']>;
+
 const loading = ref<boolean>(true);
 const recipes = ref<Recipe[]>();
 const data = computed(()=>JSON.stringify(recipes.value, null, 4));
 
-const headers = [
+const headers: ReadonlyDataTableHeader[] = [
   {
     title: "Id",
     align: "end",
