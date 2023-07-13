7f307ebb5f8801a7144fcf5e56bdd4a4848e9d26
Daniel Gee <geed@leadforensics.com>
2023-07-13 13:44:39 +0100

    Remove Menu UI to separate repo

--- MenuUI/src/services/apiHelpers.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware } from "@qdrant/openapi-typescript-fetch"
-
-export const logRequestAndResponse: Middleware = async (url, init, next) => {
-    console.log(`fetching ${url}`, { init })
-    const response = await next(url, init)
-    console.log(`fetched ${url}`, { response, init })
-    return response
-}
