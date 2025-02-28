721958df576440a8d13cf41a84189c454e4cd89e
Daniel Gee <dan@dan-gee.co.uk>
2025-02-28 16:51:45 +0000

    Fix GetExecutingAssembly sonarcloud issue

--- MenuApi/Program.cs
+++ MenuApi/Program.cs
@@ -7,6 +7,7 @@ using MenuApi.Recipes;
 using System.Reflection;
 using MenuApi.ValueObjects;
 using Menu.ApiServiceDefaults;
+using System.Runtime.CompilerServices;
 
 ValueObject.ConfigureDapperTypeHandlers();
 
@@ -15,7 +16,7 @@ builder.AddApiServiceDefaults();
 
 // Recipe specific stuff (needs putting in extension methods)
 builder.Services.ConfigureSwaggerGen(o => o.MapVogenTypesInMenuApi());
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddTransient<IUnitRepository, UnitRepository>();
 builder.Services.AddTransient<IIngredientRepository, IngredientRepository>();
