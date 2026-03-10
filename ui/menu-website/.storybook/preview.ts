ca56e83838366fa2928979c1c21c254bf43788b5
Daniel Gee <dan@dan-gee.co.uk>
2026-03-10 08:39:48 +0000

    Downgrade storybook

--- ui/menu-website/.storybook/preview.ts
+++ ui/menu-website/.storybook/preview.ts
@@ -15,6 +15,7 @@ setup((app) => {
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
