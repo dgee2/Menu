673f1eb86d79ed0576f267ea4089d0069ad8e847
Daniel Gee <dan@dan-gee.co.uk>
2026-03-10 09:04:04 +0000

    Update Storybook configuration and dependencies for improved accessibility and compatibility

--- ui/menu-website/.storybook/preview.ts
+++ ui/menu-website/.storybook/preview.ts
@@ -5,7 +5,8 @@ import { Quasar } from 'quasar';
 import '@quasar/extras/roboto-font/roboto-font.css';
 import '@quasar/extras/material-icons/material-icons.css';
 
-import 'quasar/dist/quasar.css';
+import 'quasar/src/css/index.sass'; // as suggested in https://quasar.dev/start/vite-plugin
+import '../src/css/app.scss';
 
 setup((app) => {
   app.use(Quasar, {
@@ -15,11 +16,33 @@ setup((app) => {
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#1d1d1d' },
+      ],
+    },
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: { width: '360px', height: '640px' },
+        },
+        tablet: {
+          name: 'Tablet',
+          styles: { width: '768px', height: '1024px' },
+        },
+        desktop: {
+          name: 'Desktop',
+          styles: { width: '1920px', height: '1080px' },
+        },
       },
     },
   },
